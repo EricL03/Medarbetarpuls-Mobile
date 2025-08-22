64bea3180c866eaf59718e48011a17992cb9c9ca
Eric <eric.lofgren03@gmail.com>
2025-08-22 16:35:20 +0200

    Updated to include pwa technology

---
+++ Medarbetarpuls/medarbetarapp/static/service-worker.js
@@ -0,0 +1,38 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('static-cache-v1').then((cache) => {
+      return cache.addAll([
+        '/',                         // homepage
+        '/static/manifest.json',     // manifest
+        '/static/styles.css',     // manifest
+        '/static/images/logo.png',
+        '/static/images/logo.png'
+        // add CSS, JS, and other pages if you want offline access
+      ]);
+    })
+  );
+  console.log('Service Worker: Installed');
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== 'static-cache-v1') {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  console.log('Service Worker: Activated');
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
